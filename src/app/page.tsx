2a38026beed67082a1e76a0fa54d4ccf337048ce
HOSS STORE <storehoss1@gmail.com>
2025-10-06 14:08:25 +0000

    separa a perte de cima da parte de baixo (_for element main_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,37 @@ export default function Home() {
   const bannerImage = PlaceHolderImages.find(p => p.id === 'vivasorte-banner');
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
-      <div className="flex-grow bg-background">
-        <main className="container mx-auto px-4 py-6 md:py-10">
+      <main className="flex-grow">
+        <div className="bg-white py-6 md:py-8">
+            <div className="container mx-auto px-4">
+                 <div className="max-w-2xl mx-auto">
+                    {bannerImage && (
+                    <div className="rounded-lg overflow-hidden shadow-2xl relative">
+                        <Image
+                        src={bannerImage.imageUrl}
+                        alt={bannerImage.description}
+                        width={800}
+                        height={450}
+                        className="w-full object-cover"
+                        data-ai-hint={bannerImage.imageHint}
+                        priority
+                        />
+                        <div className="absolute top-4 right-4 bg-accent text-accent-foreground px-4 py-1 rounded-md text-sm font-bold">
+                        Site Oficial
+                        </div>
+                    </div>
+                    )}
+                 </div>
+            </div>
+        </div>
+        <div className="container mx-auto px-4 py-6 md:py-10">
           <div className="max-w-2xl mx-auto">
-            {bannerImage && (
-              <div className="mb-4 rounded-lg overflow-hidden shadow-2xl relative">
-                <Image
-                  src={bannerImage.imageUrl}
-                  alt={bannerImage.description}
-                  width={800}
-                  height={450}
-                  className="w-full object-cover"
-                  data-ai-hint={bannerImage.imageHint}
-                  priority
-                />
-                 <div className="absolute top-4 right-4 bg-accent text-accent-foreground px-4 py-1 rounded-md text-sm font-bold">
-                  Site Oficial
-                </div>
-              </div>
-            )}
             <TicketSelector />
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
       <Footer />
     </div>
   );

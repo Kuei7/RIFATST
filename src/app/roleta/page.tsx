a4481c9ca0c1c07aedb3a533080093985903def4
HOSS STORE <storehoss1@gmail.com>
2025-10-13 17:05:57 +0000

    Coloque o header do site na parte da roleta

--- src/app/roleta/page.tsx
+++ src/app/roleta/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
 import { PrizeWheel } from "@/components/PrizeWheel";
 
 export default function RoletaPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <PrizeWheel />
+    <div className="flex flex-col min-h-screen bg-background">
+      <Header />
+      <main className="flex-grow flex items-center justify-center bg-gray-100 p-4">
+        <PrizeWheel />
+      </main>
+      <Footer />
     </div>
   );
 }
